ace9137b3fa89f3485e03723298b654feedefc27
Salvo Rapisarda <sdrapisarda@gmail.com>
2024-03-24 19:03:30 +0100

    Odoo XPath Tutorial - Part 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# XPath Tutorial
+
+Codice per tutorial su Xpath presente su questo blog https://www.amministraturi.it/
--- xpath_tutorial/controllers/controllers.py
+++ xpath_tutorial/controllers/controllers.py
@@ -3,7 +3,7 @@ from odoo import http
 from odoo.http import request
 
 class XpathTutorial(http.Controller):
-    @http.route('/xpath_tutorial/', auth='public')
+    @http.route('/xpath_tutorial/', auth='public', website=True)
     def index(self, **kw):
         return request.render("xpath_tutorial.index")
 
